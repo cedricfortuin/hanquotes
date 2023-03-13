b62b99d8d7e3d19a4fdf48012d1f0a06486d87bf
Cedric Fortuin <55191740+cedricfortuin@users.noreply.github.com>
2023-03-13 13:23:22 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+import RandomQuote from "@/components/RandomQuote.vue";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="Quotes van Rick"/>
+    </div>
+  </header>
+
+  <main>
+    <RandomQuote/>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/RandomQuote.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue';
+import quotes from "../../quotes.json";
+
+const amountQuotes = quotes['quotes'].length;
+const randomIndex = ref(Math.floor(Math.random() * amountQuotes));
+const quote = ref(quotes['quotes'][randomIndex.value]);
+
+setInterval(() => {
+  randomIndex.value = Math.floor(Math.random() * amountQuotes);
+  quote.value = quotes['quotes'][randomIndex.value];
+}, 5000);
+</script>
+
+<template>
+  <h2>{{ quote }}</h2>
+  <small>Quote {{ randomIndex + 1 }} out of {{ amountQuotes }} quotes.</small>
+</template>
+
+<style scoped>
+h2 {
+  text-align: center
+}
+
+
+@media (min-width: 1024px) {
+  h2 {
+    text-align: left;
+  }
+}
+
+@media (max-width: 1023px) {
+  small {
+    display: none;
+  }
+}
+</style>
